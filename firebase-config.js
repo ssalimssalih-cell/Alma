59c38010bd62c070dd179061857e450d846c31d8
ssalimssalih-cell <ssalimssalih@gmail.com>
2026-06-10 16:39:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,19 @@
-// ==================== FIREBASE CONFIGURATION - ALMA COFFEE SHOP ====================
-// À remplacer par vos propres clés après création du projet Firebase "alma-coffee-shop"
-
 const firebaseConfig = {
-    apiKey: "VOTRE_API_KEY",
-    authDomain: "alma-coffee-shop.firebaseapp.com",
-    projectId: "alma-coffee-shop",
-    storageBucket: "alma-coffee-shop.firebasestorage.app",
-    messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-    appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyDI_GLjl0xqL1-WBhI9uPrCHXi9iFyfROU",
+  authDomain: "alma-coffee-shop.firebaseapp.com",
+  projectId: "alma-coffee-shop",
+  storageBucket: "alma-coffee-shop.firebasestorage.app",
+  messagingSenderId: "267294965703",
+  appId: "1:267294965703:web:54581b103c1c5b5c16a3e2"
 };
 
-// Initialisation Firebase
+// Initialisation
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-} else {
-    firebase.app();
 }
 
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-console.log('☕ Alma Coffee Shop - Firebase OK');
+console.log("Firebase initialisé avec projet:", firebaseConfig.projectId);
